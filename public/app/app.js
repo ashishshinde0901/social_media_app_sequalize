0aa400993b3e06342dd56d3f7acb650f03b58f57
Ashish Shinde <ashishshinde0901@gmail.com>
2024-07-11 22:45:51 +0530

    completed guest login
    Signed-off-by: Ashish Shinde <ashishshinde0901@gmail.com>

--- public/app/app.js
+++ public/app/app.js
@@ -1,3 +1,23 @@
 $(()=>{
+  $('#navbar').load('./components/navbar.html',loginIfNeeded)
+  $('#footer').load('./components/footer.html')
+
 
 })
+
+function loginIfNeeded() {
+  let currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null
+  // console.log(currentUser)
+  if(!currentUser){
+    $.post('/api/users',{},(user)=>{
+      if(user){
+        console.log('logged in as ', user.username)
+        window.localStorage.user = JSON.stringify(user)
+      }
+    })
+  } else{
+    console.log('resuming session as ', currentUser.username)
+    console.log($('#nav-username').textContent)
+    $('#nav-username').text(currentUser.username)
+  }
+}
